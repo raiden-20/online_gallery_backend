0df7d61c205ca2d478bf89fed74878d45eedddd0
undine_20 <lena_sheina@bk.ru>
2024-03-28 22:12:30 +0300

    init app

---
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/OnlineGalleryBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.sheina.online_gallery_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGalleryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineGalleryBackendApplication.class, args);
+    }
+
+}
