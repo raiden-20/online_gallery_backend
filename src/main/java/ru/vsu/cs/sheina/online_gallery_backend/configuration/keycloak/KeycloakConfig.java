9f12b8bf71a72c965181fb3190a543cacd321e42
undine_20 <lena_sheina@bk.ru>
2024-04-05 00:46:41 +0300

    add keycloak config

---
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/configuration/keycloak/KeycloakConfig.java
@@ -0,0 +1,35 @@
+package ru.vsu.cs.sheina.online_gallery_backend.configuration.keycloak;
+
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+    @Value("${keycloak.url}")
+    private String keycloakUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${keycloak.clientId}")
+    private String clientId;
+
+    @Value("${keycloak.secret}")
+    private String clientSecret;
+
+    @Bean
+    Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl(keycloakUrl)
+                .realm(realm)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
+    }
+}
