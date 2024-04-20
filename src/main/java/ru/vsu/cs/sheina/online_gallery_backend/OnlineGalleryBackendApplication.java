c60be1b1af7c439863df2c2292708a8dd0fbe48b
Elena Sheina <lena_sheina@bk.ru>
2024-04-21 01:09:49 +0300

    fix timezone bug

--- src/main/java/ru/vsu/cs/sheina/online_gallery_backend/OnlineGalleryBackendApplication.java
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/OnlineGalleryBackendApplication.java
@@ -1,13 +1,22 @@
 package ru.vsu.cs.sheina.online_gallery_backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class OnlineGalleryBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineGalleryBackendApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
 }
