8e8a566c93aae7576ae0f27f58f0238f0f8f5e88
Elena Sheina <lena_sheina@bk.ru>
2024-04-25 00:49:26 +0300

    add jwt parser

--- src/main/java/ru/vsu/cs/sheina/online_gallery_backend/exceptions/AppExceptionHandler.java
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/exceptions/AppExceptionHandler.java
@@ -1,5 +1,9 @@
 package ru.vsu.cs.sheina.online_gallery_backend.exceptions;
 
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,5 +13,15 @@ public class AppExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<?> handleException(AppException appException) {
         return ResponseEntity.status(appException.getStatus()).body(appException.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleException(InvalidJwtException invalidJwtException) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Bad token");
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleException(MalformedClaimException malformedClaimException) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Bad token");
     }
 }
---
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/utils/JWTParser.java
@@ -0,0 +1,26 @@
+package ru.vsu.cs.sheina.online_gallery_backend.utils;
+
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.MalformedClaimException;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class JWTParser {
+
+    public UUID getIdFromAccessToken(String token) throws InvalidJwtException, MalformedClaimException {
+        token = token.substring(7);
+        JwtConsumer jwtConsumer = new JwtConsumerBuilder()
+                .setSkipSignatureVerification()
+                .setExpectedAudience("account")
+                .build();
+        jwtConsumer.setSkipVerificationKeyResolutionOnNone(true);
+        JwtClaims jwtClaims = jwtConsumer.processToClaims(token);
+
+        return UUID.fromString(jwtClaims.getStringClaimValue("sub"));
+    }
+}
