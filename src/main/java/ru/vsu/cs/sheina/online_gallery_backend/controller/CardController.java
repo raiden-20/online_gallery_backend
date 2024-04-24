9aad79da7cdeea228d77d08784d488faabf2f122
Elena Sheina <lena_sheina@bk.ru>
2024-04-24 18:15:26 +0300

    add card endpoints

---
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/controller/CardController.java
@@ -0,0 +1,42 @@
+package ru.vsu.cs.sheina.online_gallery_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.vsu.cs.sheina.online_gallery_backend.dto.card.CardDTO;
+import ru.vsu.cs.sheina.online_gallery_backend.dto.card.CardNewDTO;
+import ru.vsu.cs.sheina.online_gallery_backend.dto.field.IntIdDTO;
+
+@RestController
+@RequiredArgsConstructor
+public class CardController {
+
+    private final CardService cardService;
+
+    @GetMapping("/cards")
+    public ResponseEntity<?> getCards(@RequestHeader("Authorization") String token) {
+        List<CardDTO> cards = cardService.getCards(token);
+        return ResponseEntity.ok(cards);
+    }
+
+    @PostMapping("/card")
+    public ResponseEntity<?> addCard(@RequestBody CardNewDTO cardNewDTO,
+                                     @RequestHeader("Authorization") String token) {
+        cardService.addCard(cardNewDTO, token);
+        return ResponseEntity.ok("Card added successfully");
+    }
+
+    @PutMapping("/card")
+    public ResponseEntity<?> changeCard(@RequestBody CardDTO cardDTO,
+                                        @RequestHeader("Authorization") String token) {
+        cardService.changeCard(cardDTO, token);
+        return ResponseEntity.ok("Card changed successfully");
+    }
+
+    @DeleteMapping("/card")
+    public ResponseEntity<?> deleteCard(@RequestBody IntIdDTO intIdDTO,
+                                        @RequestHeader("Authorization") String token) {
+        cardService.deleteCard(intIdDTO, token);
+        return ResponseEntity.ok("Card deleted successfully");
+    }
+}
