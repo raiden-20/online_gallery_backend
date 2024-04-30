f4750b947ebe02540b063f88f75133e0d2e23e6e
Elena Sheina <lena_sheina@bk.ru>
2024-04-30 16:24:17 +0300

    add swagger in cart controller

--- src/main/java/ru/vsu/cs/sheina/online_gallery_backend/controller/CartController.java
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/controller/CartController.java
@@ -1,23 +1,42 @@
 package ru.vsu.cs.sheina.online_gallery_backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.vsu.cs.sheina.online_gallery_backend.dto.address.AddressDTO;
 import ru.vsu.cs.sheina.online_gallery_backend.dto.art.ArtShortDTO;
 import ru.vsu.cs.sheina.online_gallery_backend.dto.field.IntIdRequestDTO;
 import ru.vsu.cs.sheina.online_gallery_backend.dto.order.PurchaseDTO;
+import ru.vsu.cs.sheina.online_gallery_backend.exceptions.BadActionException;
+import ru.vsu.cs.sheina.online_gallery_backend.exceptions.BadCredentialsException;
 import ru.vsu.cs.sheina.online_gallery_backend.service.CartService;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Корзина")
 @RequestMapping("/cart")
 public class CartController {
 
     private final CartService cartService;
 
     @PostMapping()
+    @Operation(summary = "Добавить товар в корзину")
+    @ApiResponse(responseCode = "200",
+            description = "Товар добавлен в корзину",
+            content = @Content(schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "400",
+            description = "Неверные данные",
+            content = @Content(schema = @Schema(implementation = BadCredentialsException.class)))
+    @ApiResponse(responseCode = "409",
+            description = "Товар уже находится в корзине",
+            content = @Content(schema = @Schema(implementation = BadActionException.class)))
     public ResponseEntity<?> addArtInCart(@RequestBody IntIdRequestDTO intIdRequestDTO,
                                           @RequestHeader("Authorization") String token) {
         cartService.addArt(intIdRequestDTO, token);
@@ -25,6 +44,13 @@ public class CartController {
     }
 
     @DeleteMapping()
+    @Operation(summary = "Удалить товар из корзины")
+    @ApiResponse(responseCode = "200",
+            description = "Товар удален из корзины",
+            content = @Content(schema = @Schema(implementation = String.class)))
+    @ApiResponse(responseCode = "409",
+            description = "Данного товара нет в корзине",
+            content = @Content(schema = @Schema(implementation = BadActionException.class)))
     public ResponseEntity<?> deleteArtFromCart(@RequestBody IntIdRequestDTO intIdRequestDTO,
                                                @RequestHeader("Authorization") String token) {
         cartService.deleteArtFromCart(intIdRequestDTO, token);
@@ -32,12 +58,26 @@ public class CartController {
     }
 
     @GetMapping()
+    @Operation(summary = "Получить все товары в корзине")
+    @ApiResponse(responseCode = "200",
+            description = "Отправлен список товаров",
+            content = @Content(schema = @Schema(implementation = ArtShortDTO.class)))
+    @ApiResponse(responseCode = "400",
+            description = "Неверные данные",
+            content = @Content(schema = @Schema(implementation = BadCredentialsException.class)))
     public ResponseEntity<?> getCartData(@RequestHeader("Authorization") String token) {
         List<ArtShortDTO> arts = cartService.getCartData(token);
         return ResponseEntity.ok(arts);
     }
 
     @PostMapping("/buy")
+    @Operation(summary = "Оплатить товары из корзины")
+    @ApiResponse(responseCode = "200",
+            description = "Отправлен список id созданных заказов",
+            content = @Content(schema = @Schema(implementation = Integer.class)))
+    @ApiResponse(responseCode = "400",
+            description = "Неверные данные",
+            content = @Content(schema = @Schema(implementation = BadCredentialsException.class)))
     public ResponseEntity<?> buyArtsFromCart(@RequestBody PurchaseDTO purchaseDTO,
                                              @RequestHeader("Authorization") String token) {
         List<Integer> orderIds = cartService.buy(purchaseDTO, token);
