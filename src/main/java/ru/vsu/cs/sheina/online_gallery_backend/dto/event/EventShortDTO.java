48f6243fef7b666768fcd099bb658d27d1a4e32c
Elena Sheina <lena_sheina@bk.ru>
2024-05-29 14:08:39 +0300

    add event controller

---
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/dto/event/EventShortDTO.java
@@ -0,0 +1,25 @@
+package ru.vsu.cs.sheina.online_gallery_backend.dto.event;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@RequiredArgsConstructor
+public class EventShortDTO {
+
+    Integer eventId;
+
+    String photoUrl;
+
+    String name;
+
+    Timestamp startDate;
+
+    Timestamp endDate;
+
+    String description;
+
+    String status;
+}
---
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/dto/event/EventSubjectDTO.java
@@ -0,0 +1,44 @@
+package ru.vsu.cs.sheina.online_gallery_backend.dto.event;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@RequiredArgsConstructor
+public class EventSubjectDTO {
+
+    Integer subjectId;
+
+    UUID artistId;
+
+    String artistName;
+
+    String subjectName;
+
+    String photoUrl;
+
+    String status;
+
+    BigInteger price;
+
+    Integer viewCount;
+
+    String size;
+
+    Timestamp startDate;
+
+    Timestamp endDate;
+
+    Timestamp createDate;
+
+    List<String> tags;
+
+    List<String> materials;
+
+    Boolean frame;
+}
