aed0ca19f328e4ef7884b77544d93a4ade3cae00
Elena Sheina <lena_sheina@bk.ru>
2024-06-04 21:37:04 +0300

    change get event logic

--- src/main/java/ru/vsu/cs/sheina/online_gallery_backend/service/EventService.java
+++ src/main/java/ru/vsu/cs/sheina/online_gallery_backend/service/EventService.java
@@ -24,6 +24,7 @@ public class EventService {
     private final EventRepository eventRepository;
     private final ArtistRepository artistRepository;
     private final ArtRepository artRepository;
+    private final NotificationService notificationService;
     private final AuctionRepository auctionRepository;
     private final ArtPhotoRepository artPhotoRepository;
     private final AuctionPhotoRepository auctionPhotoRepository;
@@ -32,6 +33,16 @@ public class EventService {
 
     public void startEvent(EventEntity eventEntity) {
         eventEntity.setStatus("AVAILABLE");
+
+        if (eventEntity.getType().equals("ART")) {
+            List<ArtEntity> artEntities = eventSubjectRepository.findAllByEventId(eventEntity.getId()).stream()
+                    .map(event -> artRepository.findById(event.getSubjectId()).get())
+                    .toList();
+            for (ArtEntity artEntity: artEntities) {
+                notificationService.sendNewPublicArtNotification(artEntity, artistRepository.findById(artEntity.getArtistId()).get());
+            }
+        }
+
         eventRepository.save(eventEntity);
     }
 
@@ -43,15 +54,6 @@ public class EventService {
     public EventFullDTO getEvent(Integer eventId, String currentId) {
         EventEntity eventEntity = eventRepository.findById(eventId).orElseThrow(BadCredentialsException::new);
 
-        if (!currentId.equals("null") && eventEntity.getStatus().equals("WAIT")) {
-            UUID userId = UUID.fromString(currentId);
-            if (!adminService.checkAdmin(userId) && !artistRepository.existsById(userId)) {
-                throw new ForbiddenActionException();
-            }
-        } else if (eventEntity.getStatus().equals("WAIT")) {
-            throw new ForbiddenActionException();
-        }
-
         EventFullDTO dto = new EventFullDTO();
         dto.setName(eventEntity.getName());
         dto.setPhotoUrl(eventEntity.getPhotoUrl());
@@ -59,9 +61,33 @@ public class EventService {
         dto.setEndDate(eventEntity.getEndDate());
         dto.setDescription(eventEntity.getDescription());
         dto.setType(eventEntity.getType());
+
         List<EventSubjectDTO> subjectDTOs = new ArrayList<>();
 
-        List<EventSubjectEntity> subjectEntities = eventSubjectRepository.findAllByEventId(eventEntity.getId());
+        if (!currentId.equals("null") && eventEntity.getStatus().equals("WAIT")) {
+            UUID userId = UUID.fromString(currentId);
+            if (adminService.checkAdmin(userId)) {
+                List<EventSubjectEntity> subjectEntities = eventSubjectRepository.findAllByEventId(eventEntity.getId());
+                subjectDTOs = getEventSubjects(eventEntity, subjectEntities);
+            } else if (artistRepository.existsById(userId)) {
+                List<EventSubjectEntity> subjectEntities = eventSubjectRepository.findAllByEventId(eventEntity.getId()).stream()
+                        .filter(ent -> artRepository.findById(ent.getSubjectId()).get().getArtistId().equals(userId))
+                        .toList();
+                subjectDTOs = getEventSubjects(eventEntity, subjectEntities);
+            }
+        } else if (eventEntity.getStatus().equals("WAIT")) {
+            subjectDTOs = null;
+        } else {
+            List<EventSubjectEntity> subjectEntities = eventSubjectRepository.findAllByEventId(eventEntity.getId());
+            subjectDTOs = getEventSubjects(eventEntity, subjectEntities);
+        }
+        dto.setSubjects(subjectDTOs);
+        return dto;
+    }
+
+    private List<EventSubjectDTO> getEventSubjects(EventEntity eventEntity, List<EventSubjectEntity> subjectEntities) {
+        List<EventSubjectDTO> subjectDTOs = new ArrayList<>();
+
         for (EventSubjectEntity subjectEntity : subjectEntities) {
             if (eventEntity.getType().equals("ART")) {
                 ArtEntity artEntity = artRepository.findById(subjectEntity.getSubjectId()).orElseThrow(BadCredentialsException::new);
@@ -125,8 +151,7 @@ public class EventService {
                 subjectDTOs.add(subjectDTO);
             }
         }
-        dto.setSubjects(subjectDTOs);
-        return dto;
+        return subjectDTOs;
     }
 
     public List<EventShortDTO> getEvents(String currentId) {
